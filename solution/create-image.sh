5f8eadd9503e46ffee7460402adfb41fc846bcd5
fkomarzy <fkomarzy@akamai.com>
2021-06-20 22:30:20 +0200

    added necessary files for tutorial

--- solution/create-image.sh
+++
@@ -1,5 +0,0 @@
-docker build -f Dockerfile -t tip-app:latest .
-minikube start
-kubectl apply -f flask-deploy.yaml
-
-echo "Issue 'kubectl get pods' to see the status of the pods.\nNow the service should be accsessible at http://localhost:6000"
